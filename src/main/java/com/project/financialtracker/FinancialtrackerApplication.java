b1049c724649bae2457d9fed81541a802fb92008
kalyan-sendang <kalyansendang10@gmail.com>
2024-01-19 17:11:04 +0545

    Changed Jar to War file

--- src/main/java/com/project/financialtracker/FinancialtrackerApplication.java
+++ src/main/java/com/project/financialtracker/FinancialtrackerApplication.java
@@ -2,13 +2,19 @@ package com.project.financialtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class FinancialtrackerApplication {
+public class FinancialtrackerApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(FinancialtrackerApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(FinancialtrackerApplication.class);
+    }
 }
